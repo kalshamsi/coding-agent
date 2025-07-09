ddc43ba7b2b7e13db19e3dd703d5a278e7c69336
Khalfan AlShamsi <khalfanalshamsi02@gmail.com>
2025-07-09 21:36:58 +0400

    Initialized google api configuration, finished Q1

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from custom-coding-agent!")
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
 
+# Load environment variables from .env file
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
 
-if __name__ == "__main__":
-    main()
+client = genai.Client(api_key = api_key)
+
+response = client.models.generate_content(
+    model = "gemini-2.0-flash-001",
+    contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+)
+# get prompt tokens and response tokens using .usage_metadata
+prompt_tokens = response.usage_metadata.prompt_token_count
+response_tokens = response.usage_metadata.candidates_token_count
+
+print(f"Prompt tokens: {prompt_tokens}")
+print(f"Response tokens: {response_tokens}")
